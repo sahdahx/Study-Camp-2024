f1845047bd34e2dbed5f051221ffb4a50e434361
Sahda <sachimikazuki@gmail.com>
2024-05-25 12:01:35 +0700

    first init

---
+++ index.js
@@ -0,0 +1,23 @@
+const string = "145";
+const number = 145;
+const undefined = undefined;
+const object = {};
+const array = [];
+
+const student = {
+    name: John,
+    age: 20,
+    isStudent: true,
+    "last name": "William",
+};
+
+const BestAnime = ["Nisekoi", "Kimi no Nawa", "Koe no Katachi", "Naruto"];
+console.log(BestAnime[1]);
+
+if (BestAnime = BestAnime[1]) {
+    console.log("You are correct!")
+}
+
+for (let i=0; i<10; i++) {
+    console.log("Halo dunia!");
+}
